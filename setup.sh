7def75d68206a8451cd4d841809795867b8b1780
Johann Bernhardt <johann.bernhardt@tum.de>
2020-04-26 18:32:33 +0200

    First fix for build script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 wget http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10/forge-1.7.10-10.13.4.1614-1.7.10-src.zip
-unzip -o forge-1.7.10-10.13.4.1614-1.7.10-src gradle/ eclipse/ gradlew gradlew.bat
+unzip -o forge-1.7.10-10.13.4.1614-1.7.10-src gradle/wrapper/gradle-wrapper.jar eclipse/* gradlew gradlew.bat
 rm forge-1.7.10-10.13.4.1614-1.7.10-src.zip
+cp -f setup/build.gradle .
+cp -f setup/gradle-wrapper.properties gradle/wrapper/
