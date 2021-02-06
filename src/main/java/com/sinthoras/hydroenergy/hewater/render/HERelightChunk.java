86dd5d5b74daae765750d2c114501c077739cca0
Johann Bernhardt <johann.bernhardt@tum.de>
2021-02-06 14:04:25 +0100

    Code simplification

--- src/main/java/com/sinthoras/hydroenergy/hewater/render/HERelightChunk.java
+++ src/main/java/com/sinthoras/hydroenergy/hewater/render/HERelightChunk.java
@@ -1,6 +1,5 @@
 package com.sinthoras.hydroenergy.hewater.render;
 
-import java.lang.reflect.Field;
 import java.util.BitSet;
 
 import com.sinthoras.hydroenergy.proxy.HECommonProxy;
@@ -8,17 +7,8 @@ import com.sinthoras.hydroenergy.proxy.HECommonProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 
 public class HERelightChunk {
-	
-	private static Field storageArraysMapping;
-	static {
-		try {
-			storageArraysMapping = Chunk.class.getDeclaredField("storageArrays");
-			storageArraysMapping.setAccessible(true);
-		} catch(Exception e) {}
-	}
 	
 	public BitSet[] subChunks = new BitSet[16];
 	
@@ -43,58 +33,10 @@ public class HERelightChunk {
 	}
 	
 	private void patchBlockLight(int chunkX, int chunkY, int chunkZ, int x, int y, int z, float waterLevel) {
-		try {
-			Chunk chunk = Minecraft.getMinecraft().theWorld.getChunkFromChunkCoords(chunkX, chunkZ);
-			
-			ExtendedBlockStorage[] storageArrays = (ExtendedBlockStorage[]) storageArraysMapping.get(chunk);
-			ExtendedBlockStorage extendedblockstorage = storageArrays[chunkY];
-			if(extendedblockstorage == null)
-				return;
-			float diff = Math.min((chunkY << 4) - waterLevel + y, 0);
-			int lightVal = (int)(15 + diff * HECommonProxy.blockWaterStill.getLightOpacity());
-			lightVal = Math.max(lightVal, 0);
-			extendedblockstorage.setExtSkylightValue(x, y, z, lightVal);//(int)(15 - diff * HECommonProxy.blockWaterStill.getLightOpacity()));
-		} catch(Exception e) {}
+		Chunk chunk = Minecraft.getMinecraft().theWorld.getChunkFromChunkCoords(chunkX, chunkZ);
+		float diff = Math.min((chunkY << 4) - waterLevel + y, 0);
+		int lightVal = (int)(15 + diff * HECommonProxy.blockWaterStill.getLightOpacity());
+		lightVal = Math.max(lightVal, 0);
+		chunk.setLightValue(EnumSkyBlock.Sky, x, y, z, lightVal);
 	}
-	
-	/*
-	private void setLightWithoutUpdate(long key, int lightVal) {
-		try {
-			int x = (int)(key >> 32);
-			int z = (int)key;
-			int y = 64; // random dev const
-			Chunk chunk = Minecraft.getMinecraft().theWorld.getChunkFromBlockCoords(x, z);
-			
-			ExtendedBlockStorage[] storageArrays = (ExtendedBlockStorage[]) storageArraysMapping.get(chunk);
-			ExtendedBlockStorage extendedblockstorage = storageArrays[y >> 4];
-			if(extendedblockstorage == null)
-				return; // continue whatever
-			
-			// if block has sky connection
-			extendedblockstorage.setExtSkylightValue(x, y & 15, z, lightVal);
-			// else
-			extendedblockstorage.setExtBlocklightValue(x, y & 15, z, lightVal);
-		} catch(Exception e) {}
-	}
-	
-	private void getLight(long key) {
-		try {
-			int x = (int)(key >> 32);
-			int z = (int)key;
-			int y = 64; // random dev const
-			Chunk chunk = Minecraft.getMinecraft().theWorld.getChunkFromBlockCoords(x, z);
-		
-		
-			ExtendedBlockStorage[] storageArrays = (ExtendedBlockStorage[]) storageArraysMapping.get(chunk);
-			ExtendedBlockStorage extendedblockstorage = storageArrays[y >> 4];
-			if(extendedblockstorage == null)
-				return; // continue whatever
-			
-			// if block has sky connection
-			extendedblockstorage.getExtSkylightValue(x, y & 15, z);
-			//else
-			extendedblockstorage.getExtBlocklightValue(x, y & 15, z);
-		} catch(Exception e) {}
-	}
-	*/
 }
