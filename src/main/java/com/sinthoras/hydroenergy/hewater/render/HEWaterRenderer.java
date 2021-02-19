3d737778f124cbeabd8c606a119a5da1bd42353e
Johann Bernhardt <johann.bernhardt@tum.de>
2021-02-19 11:42:58 +0100

    Reverted HEWaterRenderer to show water from both directions

--- src/main/java/com/sinthoras/hydroenergy/hewater/render/HEWaterRenderer.java
+++ src/main/java/com/sinthoras/hydroenergy/hewater/render/HEWaterRenderer.java
@@ -140,6 +140,11 @@ public class HEWaterRenderer extends RenderBlockFluid {
                 tessellator.addVertexWithUV(x + 0, y + heightSW, z + 1, u1, v1);
                 tessellator.addVertexWithUV(x + 1, y + heightSE, z + 1, u4, v4);
                 tessellator.addVertexWithUV(x + 1, y + heightNE, z + 0, u3, v3);
+
+                tessellator.addVertexWithUV(x + 0, y + heightNW, z + 0, u2, v2);
+                tessellator.addVertexWithUV(x + 1, y + heightNE, z + 0, u3, v3);
+                tessellator.addVertexWithUV(x + 1, y + heightSE, z + 1, u4, v4);
+                tessellator.addVertexWithUV(x + 0, y + heightSW, z + 1, u1, v1);
             }
 
             if (renderer.renderAllFaces || renderBottom)
@@ -235,6 +240,11 @@ public class HEWaterRenderer extends RenderBlockFluid {
                     tessellator.addVertexWithUV(tx2, y + ty2, tz2, u2Flow, v2Flow);
                     tessellator.addVertexWithUV(tx2, y + 0, tz2, u2Flow, v3Flow);
                     tessellator.addVertexWithUV(tx1, y + 0, tz1, u1Flow, v3Flow);
+
+                    tessellator.addVertexWithUV(tx1, y + ty1, tz1, u1Flow, v1Flow);
+                    tessellator.addVertexWithUV(tx1, y + 0, tz1, u1Flow, v3Flow);
+                    tessellator.addVertexWithUV(tx2, y + 0, tz2, u2Flow, v3Flow);
+                    tessellator.addVertexWithUV(tx2, y + ty2, tz2, u2Flow, v2Flow);
                 }
             }
             renderer.renderMinY = 0;
