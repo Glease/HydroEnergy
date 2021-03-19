a84b9c1fd557cba911b660e29ad331724ddf57a4
Johann Bernhardt <johann.bernhardt@tum.de>
2021-03-19 10:27:25 +0100

    Fix dimension missmatch

--- src/main/java/com/sinthoras/hydroenergy/server/HEBlockQueue.java
+++ src/main/java/com/sinthoras/hydroenergy/server/HEBlockQueue.java
@@ -154,7 +154,7 @@ class HEQueueChunk {
 
 			HEPacketChunkUpdate message = new HEPacketChunkUpdate(chunk, subChunksHaveChanges);
 			for (EntityPlayerMP player : (List<EntityPlayerMP>) MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
-				if(HEConfig.dimensionIdWhitelist.contains(player.worldObj.provider.dimensionId)
+				if(chunk.worldObj.provider.dimensionId == player.worldObj.provider.dimensionId
 						&& player.getServerForPlayer().getPlayerManager().isPlayerWatchingChunk(player, chunk.xPosition, chunk.zPosition)) {
 					HE.network.sendTo(message, player);
 				}
