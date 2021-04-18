6fe57f12e017d14e3d27389d17ab712ee848fa6a
DreamMasterXXL <dream-master@gmx.net>
2021-04-18 20:21:34 +0200

    change to  GRADLETOKEN_VERSION

--- src/main/java/com/sinthoras/hydroenergy/HETags.java
+++ src/main/java/com/sinthoras/hydroenergy/HETags.java
@@ -2,7 +2,7 @@ package com.sinthoras.hydroenergy;
 
 public class HETags {
     public static final String MODID = "hydroenergy";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String HYDROENERGY = "HydroEnergy";
     public static final String COM_SINTHORAS_HYDROENERGY = "com.sinthoras.hydroenergy";
     public static final String waterLevel = "walv";
